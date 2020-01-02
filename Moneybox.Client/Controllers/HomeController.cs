0972dff7f1b8c471a220336a7cf8adf62a6023bf
ersyn_sultanbaev <ersyn_sultanbaev@mail.ru>
2020-01-02 22:28:24 +0600

    Добавьте файлы проекта.

---
+++ Moneybox.Client/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using Moneybox.Client.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Moneybox.Client.Controllers
+{
+    public class HomeController : Controller
+    {
+        CashBox _cashBox = CashBox.CashInitializer();
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddMoney(string sum)
+        {
+            _cashBox.Money += double.Parse(sum);
+            return PartialView(_cashBox);
+        }
+    }
+}
---
+++ Moneybox.Client/Models/CashBox.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Moneybox.Client.Models
+{
+    public class CashBox
+    {
+        private static CashBox _box  = new CashBox();
+        private CashBox() {}
+        public static CashBox CashInitializer() 
+        {
+            if (_box == null)
+            {
+                _box = new CashBox();
+                return _box;
+            }
+            else return _box;
+        }
+        public double Money { get; set; }
+    }
+}
